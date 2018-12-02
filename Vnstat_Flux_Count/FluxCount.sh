119c69bd377be15726cbf5577aa249a5b5752d31
CallMeReznov <30729050+CallMeReznov@users.noreply.github.com>
2018-12-02 23:03:07 +0800

    Update FluxCount.sh
    用bc格式化输出小数点，不在出现因为无法输出小数点导致的0.*MB 与0.*GB的计算精度问题

--- Vnstat_Flux_Count/FluxCount.sh
+++ Vnstat_Flux_Count/FluxCount.sh
@@ -7,9 +7,9 @@ ServerFluxCount=`vnstat -i eth0 -d |grep $CuTDateTime |awk -F "|" '{print $3}'`
 if [ ${ServerFluxCount##*[0-9]} == "MiB" ]
 then
     #打印记录的流量(单位Mib)
-    echo ${ServerFluxCount::-5}|awk '{print int($0)}'
+    echo ${ServerFluxCount::-5}|bc
 
 else
     #打印记录的流量(单位Gib转换为Mib)
-    echo ${ServerFluxCount::-5} |awk '{print int($0)*1000}'
+    echo ${ServerFluxCount::-5}*1000 |bc
 fi
