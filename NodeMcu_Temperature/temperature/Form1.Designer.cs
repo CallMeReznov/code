6cd902e760febf5ef09e7d6244c137fb86b7a6cb
Reznov <dagang97@gmail.com>
2020-12-29 14:21:18 +0800

    增加把温度用OSD的方式更新到摄像头里

--- NodeMcu_Temperature/temperature/Form1.Designer.cs
+++ NodeMcu_Temperature/temperature/Form1.Designer.cs
@@ -39,12 +39,18 @@
             this.tabPage1 = new System.Windows.Forms.TabPage();
             this.tabPage2 = new System.Windows.Forms.TabPage();
             this.button2 = new System.Windows.Forms.Button();
-            this.checkBox1 = new System.Windows.Forms.CheckBox();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
-            this.textBox3 = new System.Windows.Forms.TextBox();
             this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
             this.tabControl1.SuspendLayout();
             this.tabPage1.SuspendLayout();
             this.tabPage2.SuspendLayout();
@@ -117,6 +123,12 @@
             // 
             // tabPage2
             // 
+            this.tabPage2.Controls.Add(this.textBox5);
+            this.tabPage2.Controls.Add(this.textBox4);
+            this.tabPage2.Controls.Add(this.label6);
+            this.tabPage2.Controls.Add(this.label5);
+            this.tabPage2.Controls.Add(this.label4);
+            this.tabPage2.Controls.Add(this.checkBox2);
             this.tabPage2.Controls.Add(this.button2);
             this.tabPage2.Controls.Add(this.textBox2);
             this.tabPage2.Controls.Add(this.label1);
@@ -142,33 +154,13 @@
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
-            // checkBox1
+            // textBox2
             // 
-            this.checkBox1.AutoSize = true;
-            this.checkBox1.Location = new System.Drawing.Point(218, 124);
-            this.checkBox1.Name = "checkBox1";
-            this.checkBox1.Size = new System.Drawing.Size(22, 21);
-            this.checkBox1.TabIndex = 5;
-            this.checkBox1.UseVisualStyleBackColor = true;
-            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(87, 124);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(125, 18);
-            this.label3.TabIndex = 4;
-            this.label3.Text = "是否开启报警:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(87, 78);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(125, 18);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "报警电话号码:";
+            this.textBox2.Enabled = false;
+            this.textBox2.Location = new System.Drawing.Point(218, 27);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(50, 28);
+            this.textBox2.TabIndex = 0;
             // 
             // label1
             // 
@@ -179,20 +171,94 @@
             this.label1.TabIndex = 2;
             this.label1.Text = "报警温度阈值:";
             // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(218, 124);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(22, 21);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(87, 78);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(125, 18);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "报警电话号码:";
+            // 
             // textBox3
             // 
+            this.textBox3.Enabled = false;
             this.textBox3.Location = new System.Drawing.Point(218, 78);
             this.textBox3.Name = "textBox3";
             this.textBox3.Size = new System.Drawing.Size(257, 28);
             this.textBox3.TabIndex = 1;
             this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
             // 
-            // textBox2
+            // label3
             // 
-            this.textBox2.Location = new System.Drawing.Point(218, 27);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 28);
-            this.textBox2.TabIndex = 0;
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(87, 124);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(125, 18);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "是否开启报警:";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(218, 481);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(22, 21);
+            this.checkBox2.TabIndex = 7;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(87, 481);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(125, 18);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "是否开启更新:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(69, 378);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(143, 18);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "监控摄像头地址:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(69, 429);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(143, 18);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "监控摄像头端口:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Enabled = false;
+            this.textBox4.Location = new System.Drawing.Point(219, 375);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(256, 28);
+            this.textBox4.TabIndex = 11;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Enabled = false;
+            this.textBox5.Location = new System.Drawing.Point(218, 426);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(60, 28);
+            this.textBox5.TabIndex = 12;
             // 
             // Form1
             // 
@@ -234,6 +300,12 @@
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.CheckBox checkBox1;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
     }
 }
 
--- NodeMcu_Temperature/temperature/Form1.cs
+++ NodeMcu_Temperature/temperature/Form1.cs
@@ -13,6 +13,8 @@ using System.Threading;
 using System.IO.Ports;
 using System.Configuration;
 using System.Diagnostics;
+using ConfigCSharpDemo;
+using System.Runtime.InteropServices;
 
 namespace temperature
 {
@@ -24,7 +26,11 @@ namespace temperature
         dynamic api;
         Response response;
         dynamic param;
-        dynamic ns ;
+        dynamic ns;
+        public Int32 m_lUserID = -1;
+        private CHCNetSDK.NET_DVR_SHOWSTRING_V30 DeviceOsd;
+        string temp;
+        CHCNetSDK.NET_DVR_DEVICEINFO_V30 DeviceInfo = new CHCNetSDK.NET_DVR_DEVICEINFO_V30();
         public Form1()
         {
             System.Windows.Forms.Control.CheckForIllegalCrossThreadCalls = false; 
@@ -40,11 +46,15 @@ namespace temperature
             var appSettings = ConfigurationManager.AppSettings;
             textBox2.Text = appSettings["Temp"] ?? "Not Found";
             textBox3.Text = appSettings["Phonenum"] ?? "Not Found";
+            textBox4.Text = appSettings["WebCamip"] ?? "Not Found";
+            textBox5.Text = appSettings["WebCamport"] ?? "Not Found";
+
         }
 
 
         private void button1_Click(object sender, EventArgs e)
         {
+
 
             if (button1.Text == "Start")
             {
@@ -59,8 +69,7 @@ namespace temperature
                 this.notifyIcon1.Text = "Working";
                 comboBox1.Enabled = false;
                 checkBox1.Enabled = false;
-                textBox2.Enabled = false;
-                textBox3.Enabled = false;
+                checkBox2.Enabled = false;
             }
             else
             {
@@ -71,8 +80,7 @@ namespace temperature
                 this.notifyIcon1.Text = "Idle";
                 comboBox1.Enabled = true;
                 checkBox1.Enabled = true;
-                textBox2.Enabled = true;
-                textBox3.Enabled = true;
+                checkBox2.Enabled = true;
             }
         }
 
@@ -108,12 +116,16 @@ namespace temperature
 
         public void Zget()
         {
+            var appSettings = ConfigurationManager.AppSettings;
+            string WebCamip = appSettings["WebCamip"] ?? "Not Found";
+            string WebCamport = appSettings["WebCamport"] ?? "Not Found";
+            string WebCamUser = appSettings["WebCamUser"] ?? "Not Found";
+            string WebCamPasswd = appSettings["WebCamPasswd"] ?? "Not Found";
             Process Rp = new Process();
             Rp.StartInfo.FileName = @"adb";
             Rp.StartInfo.Arguments = @"shell am start -a android.intent.action.CALL -d tel:" + textBox3.Text;
             Rp.StartInfo.UseShellExecute = false;
             Rp.StartInfo.CreateNoWindow = true;
-            
             serialPort1.PortName = comboBox1.Text;
             try
             {
@@ -133,14 +145,43 @@ namespace temperature
                     try
                     {
                         response = api.Call("history.get", param);
-                        string aaaa = response.Result[0].value;
-
-                        
+                        temp = response.Result[0].value;
                         if (ns != response.Result[0].clock)
                         {
                             textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  当前温度:" + response.Result[0].value + "\n\r");
-                            serialPort1.WriteLine(aaaa);
+                            serialPort1.WriteLine(temp);
                             ns = response.Result[0].clock;
+                            if (checkBox2.Checked == true)
+                            {
+
+                                CHCNetSDK.NET_DVR_Init();
+                                m_lUserID = CHCNetSDK.NET_DVR_Login_V30(textBox4.Text, UInt16.Parse(textBox5.Text), WebCamUser, WebCamPasswd, ref DeviceInfo);
+                                //textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  登录错误,错误代码:"+ CHCNetSDK.NET_DVR_GetLastError() + "\n\r");
+                                UInt32 dwReturn = 0;
+                                Int32 nSize = Marshal.SizeOf(DeviceOsd);
+                                IntPtr ptrShowStrCfg = Marshal.AllocHGlobal(nSize);
+                                Marshal.StructureToPtr(DeviceOsd, ptrShowStrCfg, false);
+                                if (!CHCNetSDK.NET_DVR_GetDVRConfig(m_lUserID, CHCNetSDK.NET_DVR_GET_SHOWSTRING_V30, 1, ptrShowStrCfg, (UInt32)nSize, ref dwReturn))
+                                {
+                                    textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  获取失败,错误代码:" + CHCNetSDK.NET_DVR_GetLastError() + "\n\r");
+                                }
+                                DeviceOsd = (CHCNetSDK.NET_DVR_SHOWSTRING_V30)Marshal.PtrToStructure(ptrShowStrCfg, typeof(CHCNetSDK.NET_DVR_SHOWSTRING_V30));
+                                temp = DateTime.Now.ToLongTimeString().ToString() + "  " + temp + "℃";
+                                DeviceOsd.struStringInfo[0].wShowString = 1;
+                                DeviceOsd.struStringInfo[0].sString = temp;
+                                DeviceOsd.struStringInfo[0].wStringSize = (ushort)temp.Length;
+                                DeviceOsd.struStringInfo[0].wShowStringTopLeftX = UInt16.Parse("50");
+                                DeviceOsd.struStringInfo[0].wShowStringTopLeftY = UInt16.Parse("650");
+                                Marshal.StructureToPtr(DeviceOsd, ptrShowStrCfg, false);
+                                if (!CHCNetSDK.NET_DVR_SetDVRConfig(m_lUserID, CHCNetSDK.NET_DVR_SET_SHOWSTRING_V30, 1, ptrShowStrCfg, (UInt32)nSize))
+                                {
+                                    textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  设置失败,错误代码:" + CHCNetSDK.NET_DVR_GetLastError() + "\n\r");
+                                }
+
+                                Marshal.FreeHGlobal(ptrShowStrCfg);
+                                CHCNetSDK.NET_DVR_Logout(m_lUserID);
+                                CHCNetSDK.NET_DVR_Cleanup();
+                            }
                             if (checkBox1.Checked == true)
                             {
                                 if (Convert.ToDouble(response.Result[0].value) > Convert.ToDouble(textBox2.Text))
@@ -158,8 +199,8 @@ namespace temperature
                     catch
                     {
                         textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  接口数据获取异常\n\r");
-
                     }
+
                     Thread.Sleep(60000);
                 }
             }
@@ -206,11 +247,9 @@ namespace temperature
         {
             textBox1.Text = "";
         }
-
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
         }
-
         private void button2_Click(object sender, EventArgs e)
         {
             Process RpTest = new Process();
@@ -220,7 +259,6 @@ namespace temperature
             RpTest.StartInfo.CreateNoWindow = true;
             RpTest.Start();
         }
-
         private void checkBox1_CheckedChanged(object sender, EventArgs e)
         {
             if (checkBox1.Checked == true)
@@ -232,13 +270,9 @@ namespace temperature
                 textBox1.AppendText("\n\r" + DateTime.Now.ToString() + "  关闭电话报警\n\r");
             }
         }
-
         private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
         {
 
         }
     }
-
-
 }
-
